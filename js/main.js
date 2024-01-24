767b8af9bf09d7c4f6339c4509a658b04746e576
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2024-01-24 17:13:54 +0100

    New commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Jquery is a js library and can also be used for DOM manipulation and event handling, animation and AJAX
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 42.361145, lng: -71.057083 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc,
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Sticky menu background
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling => $ is used to access Jquery methods to create smooth scrolling effect on a web pages
+$("#navbar a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
